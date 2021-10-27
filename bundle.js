4358686acb9782bee5df949e8fae0b3829aa92df
Hika <61226308+hikarinoeclipsev2@users.noreply.github.com>
2021-10-27 19:31:15 +0700

    lol

---
+++ bundle.js
@@ -0,0 +1,11 @@
+import {MDCRipple} from '@material/ripple/index';
+const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+import {MDCTopAppBar} from '@material/top-app-bar';
+
+// Instantiation
+const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+const topAppBar = new MDCTopAppBar(topAppBarElement);
+
+import {MDCBanner} from '@material/banner';
+const banner = new MDCBanner(document.querySelector('.mdc-banner'));
